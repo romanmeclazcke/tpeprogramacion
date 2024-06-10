6c3241357b7792c4ff8ed28e58b968cbe84cad8c
milagros alvarez <milagrosalvarez2604@gmail.com>
2024-06-10 12:05:30 -0300

    agrego descripciones de los métodos greedy para finalizar el trabajo

--- ServicioMejorDistribucionGreedy.java
+++ ServicioMejorDistribucionGreedy.java
@@ -16,6 +16,11 @@ public class ServicioMejorDistribucionGreedy {
         this.cantidadEstados = 0;
     }
 
+    //en el método público, para realizar la funcionalidad usando una estrategia greedy en primer lugar creamos un hashtable
+    // de procesadores y tareas al cuál llamamos camino actual y lo cargamos con todos los procesadores del sistem.
+    //Luego asignamos al atributo de la clase el limite de tiempo enviado por el usuario, creamos un hashtable resultado, al cual
+    // le asignamos un hashtable proveniente de la copia de la mejor distribución de tareas en procesadores, y finalmnete 
+    // retornamos un objeto de tipo resultado con todos los datos asociados a la resolución para mantener el encapsulamiento.
     public Resultado getBestDistribution(int x) {
         Hashtable<Procesadores, ArrayList<Tarea>> caminoActual = new Hashtable<>();
         for (Procesadores procesador : this.procesadores) {
@@ -29,16 +34,19 @@ public class ServicioMejorDistribucionGreedy {
         
     }
 
+    // usando la estrategia de greedy planteamos la distribución de forma que iterando cada tarea creábamos nuevos estados candidatos
+    // y planteamos un algoritmo que dada una tarea se recorren todos procesadores y se analiza, teniendo en cuenta la distribución 
+    // formada hasta el momento, cuál es el procesador que quedaría con menor tiempo si se le asigna dicha tarea.
     private Hashtable<Procesadores, ArrayList<Tarea>> getBestDistribution(Hashtable<Procesadores, ArrayList<Tarea>> caminoActual, ArrayList<Tarea> tareas) {
         for (Tarea t : tareas) {
             this.cantidadEstados++;
             Procesadores p = this.obtenerMejorProcesador(t, caminoActual);
             if (p != null) {
                 caminoActual.get(p).add(t);
-                p.setTiempo_ejecucion(p.getTiempo_ejecucion() + t.getTiempo_ejecucion());
+                p.setTiempo_ejecucion(p.getTiempo_ejecucion() + t.getTiempo_ejecucion()); // actualizamos el tiempo de ejecución
                 if (p.getTiempo_ejecucion() > this.tiempoActual) {
-                    this.tiempoActual = p.getTiempo_ejecucion();
-                }
+                    this.tiempoActual = p.getTiempo_ejecucion(); // si el nuevo tiempo es mayor, actualizamos el atributo
+                }                                               // que contenía el tiempo más alto del momento
             }else{
                 caminoActual.clear();
                 return caminoActual;
@@ -47,6 +55,9 @@ public class ServicioMejorDistribucionGreedy {
         return caminoActual;
     }
 
+    // en este método por cada procesador corroboramos si cumple las condiciones para procesar la tarea especificada y a los 
+    // posibles candidatos los guardamos en una lista auxiliar, para luego compararlos entre todos y devolver aquel procesador
+    // que acumula menos tiempo total en procesar sus tareas.
     private Procesadores obtenerMejorProcesador(Tarea t, Hashtable<Procesadores, ArrayList<Tarea>> caminoActual) {
         ArrayList<Procesadores> aux = new ArrayList<>();
         for (Procesadores p : this.procesadores) {
@@ -85,6 +96,8 @@ public class ServicioMejorDistribucionGreedy {
 
     }
 
+    // este método cumple la misma funcionalidad que el método homónimo del servicio con backtracking, mantener el encapsulamiento
+    // de los datos devolviendo una nueva copia de los mismos.
     private Hashtable<Procesadores, ArrayList<Tarea>> getCopiaMejor(Hashtable<Procesadores, ArrayList<Tarea>> camino) {
         Hashtable<Procesadores, ArrayList<Tarea>> copiaMejor = new Hashtable<>();
         for (Procesadores p : camino.keySet()) {
